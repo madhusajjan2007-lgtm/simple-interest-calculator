c0f330e78b9556636fae92cad41b1d317c1138d6
madhura sajjan <madhusajjan2007@gmail.com>
2026-03-31 18:45:37 +0530

    Create fibonacci_recursion.c

---
+++ fibonacci_recursion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Recursive function to find Fibonacci number
+int fibonacci(int n) {
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n, i;
+
+    // Input
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    // Output Fibonacci series
+    printf("Fibonacci Series: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+
+    return 0;
+}
